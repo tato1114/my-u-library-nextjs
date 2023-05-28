0f7153e1a3c67be63f362bad6d2d4cf240e58e56
Edgardo Moreno <eleonel1496@gmail.com>
2023-05-27 23:40:24 -0600

    🐛 Hides mark as returned button from normal user

--- app/check_outs/page.jsx
+++ app/check_outs/page.jsx
@@ -105,11 +105,14 @@ function CheckOuts() {
                     </select>
                     <Button className="ml-4" onClick={(e) => search()}>Search</Button>
                 </div>
-                <div className="inline-flex rounded-md shadow-sm ml-8" role="group">
-                    <button type="button" onClick={returnBook} className="rounded-lg px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
-                        Mark as returned
-                    </button>
-                </div>
+
+                {session?.user?.role == 'librarian' &&
+                    <div className="inline-flex rounded-md shadow-sm ml-8" role="group">
+                        <button type="button" onClick={returnBook} className="rounded-lg px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700">
+                            Mark as returned
+                        </button>
+                    </div>
+                }
             </div>
             <Table
                 columns={columns}
